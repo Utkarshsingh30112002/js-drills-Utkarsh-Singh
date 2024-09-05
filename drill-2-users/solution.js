ee3237d53ef8db4a096912d86f1cd164cc050f50
Utkarsh Singh <utkarshsingh30112002@gmail.com>
2024-09-05 15:32:56 +0530

    d2-qs1 solved

---
+++ drill-2-users/solution.js
@@ -0,0 +1,48 @@
+function validArgument(users){
+    if(!users){
+        console.log("Pass a valid argument");
+        return false;
+    }
+    let arr=Object.keys(users);
+    if(users.length==0){
+        console.log("Pass a valid argument");
+        return false;
+    }
+    for(let i=0;i<arr.length;i++){
+        if(!Number.isInteger(users[arr[i]].age)||users[arr[i]].age<0){
+            console.log(`Check age of ${arr[i]}`);
+            return false;
+        }
+        if(!users[arr[i]].desgination||users[arr[i]].desgination.trim().length==0){
+            console.log("Check desgination of "+arr[i]);
+            return false;
+        }
+        if(!users[arr[i]].qualification||users[arr[i]].qualification.trim().length==0){
+            console.log("Check qualification of "+arr[i]);
+            return false;
+        }
+        if(!users[arr[i]].nationality||users[arr[i]].nationality.trim().length==0){
+            console.log("Check nationality of "+arr[i]);
+            return false;
+        }
+        if(!Array.isArray(users[arr[i]].interests)||users[arr[i]].interests.length==0){
+            console.log("Check interests of "+arr[i]);
+            return false;
+        }
+    }
+    return true;
+}
+//Wrote the valid argument which checks the formet of users object other wise it gives error message and undefined
+
+// Q1 Find all users who are interested in playing video games.
+function intrestInVideoGames(users){
+    if(!validArgument(users))return;
+    let reg=/video games/i;
+    let arr=[];
+    for(let user in users){
+        if(reg.test(users[user]["interests"]?.[0]))arr.push(user);
+    }
+    return arr;
+}
+
+export{intrestInVideoGames}//,inGermany,withMasters,groupUsers};
---
+++ drill-2-users/test.js
@@ -0,0 +1,7 @@
+import {intrestInVideoGames}from "./solution.js";//,inGermany,withMasters,groupUsers} 
+import {users} from "./users.js";
+
+// Q1 Find all users who are interested in playing video games.
+console.log(intrestInVideoGames(users));
+intrestInVideoGames({});
+intrestInVideoGames();
