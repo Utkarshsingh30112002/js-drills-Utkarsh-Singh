533ac8db377aa0a2b7a6b1f11ddcd4e4639b8abb
Utkarsh Singh <utkarshsingh30112002@gmail.com>
2024-09-24 18:18:28 +0530

    self made promise drill completed

--- selfmade-promise-drill/tests/testProblem1.js
+++ selfmade-promise-drill/tests/testProblem1.js
@@ -1,2 +1,4 @@
 import { createDirThenCreateFilesThenDeleteFiles } from "../problem1.js";
-createDirThenCreateFilesThenDeleteFiles("./randomFiles",2)
+let n=Math.floor((Math.random()*10))+1  // genrate random numbers in range 1-10
+console.log(n)
+createDirThenCreateFilesThenDeleteFiles("./randomFiles",n)
